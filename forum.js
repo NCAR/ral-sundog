6abbc03131f50a6fd2458eeabd9c06cd250c9cfc
Lara Ziady <ziady@ucar.edu>
2025-05-09 12:14:35 -0600

    forum styles

--- forum.js
+++ forum.js
@@ -1,5 +1,6 @@
-remove the row class within <div id="forums" class="thread type-idealist">
-Add span3 to <section class="post-section">
-Add span3 to <div class="tl-eventcard pull-left">
+// Forums JS for RAL Sundog
 
-jQuery('div.view-taxonomy-term-list-card-grid .view-content').addClass('row row-cols-1 row-cols-sm-2 row-cols-md-4 g-5 grid-list');
+jQuery(html).find(div.span1.hidden-phone.hidden-tablet.tl-person-stream).remove();
+jQuery(html).find('div.thread.type-idealist').remove('div.row');
+jquery('section.post-section').addClass('span3');
+jquery('div.tl-eventcard.pull-left').addClass('span3');
