52f7e98b2c597fb18ea9556c5590822f63fd2f79
Lara Ziady <ziady@ucar.edu>
2025-05-09 12:12:37 -0600

    initial commit

---
+++ forum.js
@@ -0,0 +1,5 @@
+remove the row class within <div id="forums" class="thread type-idealist">
+Add span3 to <section class="post-section">
+Add span3 to <div class="tl-eventcard pull-left">
+
+jQuery('div.view-taxonomy-term-list-card-grid .view-content').addClass('row row-cols-1 row-cols-sm-2 row-cols-md-4 g-5 grid-list');
