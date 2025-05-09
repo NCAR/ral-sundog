6878b003f7219a1950926c111e02d011b87f1910
Lara Ziady <ziady@ucar.edu>
2025-05-09 13:18:26 -0600

    corrections to js

--- forum.js
+++ forum.js
@@ -1,6 +1,6 @@
 // Forums JS for RAL Sundog
 
-jQuery(html).find(div.span1.hidden-phone.hidden-tablet.tl-person-stream).remove();
-jQuery(html).find('div.thread.type-idealist').remove('div.row');
-jquery('section.post-section').addClass('span3');
-jquery('div.tl-eventcard.pull-left').addClass('span3');
+$(this).find('div.span1.hidden-phone.hidden-tablet.tl-person-stream').remove();
+$(this).find('div.thread.type-idealist').remove('div.row');
+$('section.post-section').addClass('span3');
+$('div.tl-eventcard.pull-left').addClass('span3');
