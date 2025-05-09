3d8cf8b6674e2ac42b749e95fd030e703cfba992
Lara Ziady <ziady@ucar.edu>
2025-05-09 13:49:28 -0600

    changed order of events in js

--- forum.js
+++ forum.js
@@ -1,6 +1,8 @@
 // Forums JS for RAL Sundog
 
-$('div.hidden-phone.hidden-tablet.tl-person-stream.span1').empty();
-$('div.thread.type-idealist').remove('div.row');
+$('div.hidden-phone.hidden-tablet.tl-person-stream.span1').remove();
+$('div.thread.type-idealist').removeClass('row');
 $('section.post-section').addClass('span3');
 $('div.tl-eventcard.pull-left').addClass('span3');
+$('section.post-section.span3 div.row').removeClass('row');
+$('section.post-section.span3 div.span8').removeClass('span8');
